c559a7e04b76d8bc04b25d82fc4533ac2aa02601
bass <naveter@gmail.com>
2020-03-11 17:04:49 +0300

    calculations and STD redirections

---
+++ src/calcSTDredirect.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+let RESULT1=$1+$2
+echo $1+$2=$RESULT1 ' -> # let RESULT1=$1+$2'
+declare -i RESULT2
+RESULT2=$1+$2
+echo $1+$2=$RESULT2 ' -> # declare -i RESULT2; RESULT2=$1+$2'
+echo $1+$2=$(($1 + $2)) ' -> # $(($1 + $2))'
+
+
+# There are two formats for arithmetic expansion: $[ expression ]
+# and $(( expression #)) its your choice which you use
+echo 4 + 5 = $((4 + 5))
+echo 7 - 7 = $[ 7 - 7 ]
+
+echo -e "Please enter two numbers \c"
+
+# get floating point number
+floating_point_number=3.3446
+echo $floating_point_number
+# round floating point number with bash
+for bash_rounded_number in $(printf %.2f "3.3446"); do
+    echo "Rounded number with bash:" $bash_rounded_number
+done
+
+# STDERR and STDOUT into the same out
+# 2> STDERR_STDOUT 1>&2
+# &> STDERR_STDOUT
+# >& STDERR_STDOUT
+
+
+
