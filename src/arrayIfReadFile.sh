0548f9a5f8ff1d7872502b33061445cfde5f6173
naveter <naveter@gmail.com>
2021-04-08 13:22:51 +0300

    test token

--- src/arrayIfReadFile.sh
+++ src/arrayIfReadFile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Declare array with 4 elements
+#Declare array with 4 elements ...
 ARRAY=( 'Debian Linux' 'Redhat Linux' Ubuntu Linux )
 # get number of elements in the array
 ELEMENTS=${#ARRAY[@]}
